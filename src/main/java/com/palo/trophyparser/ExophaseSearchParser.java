f772846c5a0146c4f68c94996e7dff6997aca1ec
Paweł Prędki <pawel@tellyo.com>
2020-11-11 22:03:26 +0100

    fix for Vita-only games

--- src/main/java/com/palo/trophyparser/ExophaseSearchParser.java
+++ src/main/java/com/palo/trophyparser/ExophaseSearchParser.java
@@ -3,9 +3,10 @@ package com.palo.trophyparser;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-
+import java.util.Set;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,59 +14,62 @@ import org.jsoup.select.Elements;
 
 public class ExophaseSearchParser {
 
-	private String searchTerm;
-	private int system;
+  private String searchTerm;
+  private int system;
 
-	public ExophaseSearchParser(String searchTerm, int system) throws UnsupportedEncodingException {
-		this.searchTerm = URLEncoder.encode(searchTerm.toLowerCase(), "UTF-8");
-		this.system = system;
-	}
+  public ExophaseSearchParser(String searchTerm, int system) throws UnsupportedEncodingException {
+    this.searchTerm = URLEncoder.encode(searchTerm.toLowerCase(), "UTF-8");
+    this.system = system;
+  }
 
-	public List<Game> search() throws IOException {
-		List<Game> games = new LinkedList<Game>();
-		int index = 1;
+  public List<Game> search() throws IOException {
+    List<Game> games = new LinkedList<Game>();
+    int index = 1;
 
-		Document d = Jsoup.connect(App.EXOPHASE_DOMAIN + "/search?s=" + this.searchTerm).get();
-		Element main = d.select("ul.list-unordered-base").first();
-		if (null == main) {
-			System.out.println("MAIN NOT FOUND");
-			return games;
-		}
-		String urlSuffix = "";
-		String platformFilter = "";
-		switch (this.system) {
-		case App.SYSTEM_XBOX_FLAG:
-			urlSuffix = "achievements";
-			platformFilter = "xbox";
-			break;
-		case App.SYSTEM_PS_FLAG:
-			urlSuffix = "trophies";
-			platformFilter = "ps";
-			break;
-		default:
-			return games;
-		}
-		Elements gameRows = main.select("li");
-		for (Element row : gameRows) {
-			Element urlCell = row.select("a").first();
-			String gameUrl = urlCell.attr("href");
-			Element titleCell = row.select("div").first().select("h3").first().select("a").first();
-			String gameTitle = titleCell.text();
-			Elements platformCells = row.select("div.inline-pf");
-			String gamePlatform = "";
-			for (Element platformCell : platformCells) {
-				if (!gamePlatform.isEmpty()) {
-					gamePlatform += ", ";
-				}
-				gamePlatform += platformCell.text();
-			}
-			if (gamePlatform.toLowerCase().contains(platformFilter)) {
-				System.out.println(index++ + " : " + gameTitle + " | " + gamePlatform);
-				Game g = new Game(gameTitle, gamePlatform, gameUrl);
-				games.add(g);
-			}
-		}
-		return games;
-	}
+    Document d = Jsoup.connect(App.EXOPHASE_DOMAIN + "/search?s=" + this.searchTerm).get();
+    Element main = d.select("ul.list-unordered-base").first();
+    if (null == main) {
+      System.out.println("MAIN NOT FOUND");
+      return games;
+    }
+    String urlSuffix = "";
+    Set<String> platformFilter = new HashSet<>();
+    switch (this.system) {
+      case App.SYSTEM_XBOX_FLAG:
+        urlSuffix = "achievements";
+        platformFilter.add("xbox");
+        break;
+      case App.SYSTEM_PS_FLAG:
+        urlSuffix = "trophies";
+        platformFilter.add("ps");
+        platformFilter.add("vita");
+        break;
+      default:
+        return games;
+    }
+    Elements gameRows = main.select("li");
+    for (Element row : gameRows) {
+      Element urlCell = row.select("a").first();
+      String gameUrl = urlCell.attr("href");
+      Element titleCell = row.select("div").first().select("h3").first().select("a").first();
+      String gameTitle = titleCell.text();
+      Elements platformCells = row.select("div.inline-pf");
+      String gamePlatform = "";
+      for (Element platformCell : platformCells) {
+        if (!gamePlatform.isEmpty()) {
+          gamePlatform += ", ";
+        }
+        gamePlatform += platformCell.text();
+      }
+      final String lowerGamePlatform = gamePlatform.toLowerCase();
+      boolean found = platformFilter.stream().anyMatch(x -> lowerGamePlatform.contains(x));
+      if (found) {
+        System.out.println(index++ + " : " + gameTitle + " | " + gamePlatform);
+        Game g = new Game(gameTitle, gamePlatform, gameUrl);
+        games.add(g);
+      }
+    }
+    return games;
+  }
 
 }
